ec73953d6acb62ba06df0b265fba1823bd391596
Byron Wade <bcw1995@gmail.com>
2023-02-07 20:35:06 -0800

    more

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+"use client"
+import './globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+  RecoilRoot
+} from 'recoil';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+      <html lang="en">
+        {/*
+          <head /> will contain the components returned by the nearest parent
+          head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+        */}
+        <head />
+        <body>
+          <RecoilRoot>
+            <ChakraProvider>
+              {children}
+            </ChakraProvider>
+          </RecoilRoot>
+        </body>
+      </html>
+   
+  )
+}
