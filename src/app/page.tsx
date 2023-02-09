96e6fc23a3606991b517bb47aa31585905bc9078
Byron Wade <bcw1995@gmail.com>
2023-02-08 23:57:51 -0800

    added alot of the core stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-import SessionProvider from './componenets/SessionProvider'
-import {getServerSession} from "next-auth"
-import { authOptions } from '../pages/api/auth/[...nextauth]';
-import Login from './componenets/Login/Login';
-import Logout from './componenets/Logout/Logout';
-import Test from './componenets/Test';
+import Header from "./componenets/Header/Header";
 
 export default async function Home() {
-	const session = await getServerSession(authOptions)
 	return (
-	<div>
-		<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		<SessionProvider session={session}>
-			{!session ? (
-				<Login />
-			) : (
-				<div>
-					<Logout />
-					<hr />
-					<Test />
-				</div>
-			)}
-		</SessionProvider>
-	</div>);
+		<div>
+			<Header />
+		</div>
+	);
 }
