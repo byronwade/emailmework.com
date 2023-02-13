1e88788e23e8fbd04dc59390bb244176f6fa90c2
Byron Wade <bcw1995@gmail.com>
2023-02-12 19:00:31 -0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import SessionProvider from './componenets/SessionProvider'
-import {getServerSession} from "next-auth"
-import { authOptions } from '../pages/api/auth/[...nextauth]';
-import Login from './componenets/Login/Login';
-import Logout from './componenets/Logout/Logout';
-import Test from './componenets/Test';
-
-export default async function Home() {
-	const session = await getServerSession(authOptions)
-	return (
-	<div>
-		<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		<SessionProvider session={session}>
-			{!session ? (
-				<Login />
-			) : (
-				<div>
-					<Logout />
-					<hr />
-					<Test />
-				</div>
-			)}
-		</SessionProvider>
-	</div>);
-}
-=======
-
 export default async function Home() {
 	return (
 		<div>
@@ -33,4 +5,3 @@ export default async function Home() {
 		</div>
 	);
 }
->>>>>>> b9e6453f
