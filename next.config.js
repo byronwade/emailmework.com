55ee853cabd15d137bbedfee6b9e856b6d0c5926
Byron Wade <bcw1995@gmail.com>
2023-02-20 00:07:12 -0800

    added more

--- next.config.js
+++ next.config.js
@@ -88,4 +88,4 @@ const securityHeaders = [
 	},
 ];
 
-module.exports = nextConfig;
+export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default async function Home() {
+"use client";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import type { DocumentData } from "firebase/firestore";
+import { db } from "../../firebase.js";
+import { useState, useEffect } from "react";
+
+export default function Home() {
+	const [adminUsers, setAdminUsers] = useState<DocumentData[]>([]);
+
+	useEffect(() => {
+		const unsub = onSnapshot(collection(db, "Admin Users"), (snapshot) => {
+			const adminUsersData = snapshot.docs.map((doc) => ({
+				id: doc.id,
+				...doc.data(),
+			}));
+			setAdminUsers(adminUsersData);
+		});
+
+		return unsub;
+	}, []);
+
 	return (
 		<div>
-			<div>home page</div>
+			{adminUsers.map((user) => (
+				<div key={user.admin_id}>
+					<h2 className="h2">{user.name}</h2>
+					<p>{user.email}</p>
+				</div>
+			))}
 		</div>
 	);
 }
