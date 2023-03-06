eb842c8ca46b7ddae519340264d1b7b3e9da8938
Byron Wade <bcw1995@gmail.com>
2023-03-05 18:42:11 -0800

    test

--- firebase/db.js
+++ firebase/db.js
@@ -1,183 +1,321 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { db } from "./firebase/firebase.js/index.js.js";
+import { db } from "./firebase.js";
 import { faker } from "@faker-js/faker";
 import { v4 as uuidv4 } from "uuid";
 
-const jobID = uuidv4(); // Generate a unique job ID
-const clientID = uuidv4(); // Generate a unique user ID
-const companyID = uuidv4(); // Generate a unique user ID
-const orderID = uuidv4(); // Generate a unique user ID
-const adminID = uuidv4(); // Generate a unique user ID
-const notificationID = uuidv4(); // Generate a unique user ID
-const paymentID = uuidv4(); // Generate a unique user ID
-const reviewID = uuidv4(); // Generate a unique user ID
-const scoreID = uuidv4(); // Generate a unique user ID
+// Insert fake clients
+async function insertFakeClients(numClients) {
+	const clients = [];
 
-let newCompanyID = "";
-let newClientID = "";
+	for (let i = 0; i < numClients; i++) {
+		clients.push({
+			client_id: uuidv4(),
+			name: faker.name.fullName(),
+			email: faker.internet.email(),
+			created_at: new Date(),
+			updated_at: new Date(),
+			phone_number: faker.phone.number(),
+			profile_picture: faker.internet.avatar(),
+			location: faker.address.city(),
+			preferred_services: ["Cleaning", "Gardening"],
+			preferred_location: faker.address.city(),
+		});
+	}
 
-// Create "Admin Users" collection
-const adminUsersCollection = collection(db, "Admin Users");
+	try {
+		const batch = db.batch();
+		clients.forEach((client) => {
+			const docRef = db.collection("clients").doc(client.client_id);
+			batch.set(docRef, client);
+		});
+		await batch.commit();
+		console.log(`${clients.length} clients inserted successfully.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
 
-// Add fields to "Admin Users" collection
-addDoc(adminUsersCollection, {
-	admin_id: adminID,
-	name: faker.name.fullName(),
-	email: faker.internet.email(),
-	password: faker.internet.password(),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-});
+// Insert fake companies
+async function insertFakeCompanies(numCompanies) {
+	const companies = [];
+	for (let i = 0; i < numCompanies; i++) {
+		companies.push({
+			company_id: uuidv4(),
+			name: faker.company.name(),
+			email: faker.internet.email(),
+			created_at: new Date(),
+			updated_at: new Date(),
+			company_structure: "LLC",
+			company_ein: faker.finance.account(),
+			company_license_number: faker.finance.account(),
+			address: faker.address.streetAddress(),
+			phone_number: faker.phone.number(),
+			profile_picture: faker.internet.avatar(),
+			company_website: faker.internet.url(),
+			verified: true,
+			slug: faker.lorem.slug(),
+			link: faker.internet.url(),
+			size: 100,
+			founded: 2000,
+			social_media: ["https://facebook.com", "https://twitter.com"],
+		});
+	}
 
-// Create "Companies" collection
-const companiesCollection = collection(db, "Companies");
+	try {
+		const batch = db.batch();
+		companies.forEach((company) => {
+			const docRef = db.collection("companies").doc(company.company_id);
+			batch.set(docRef, company);
+		});
+		await batch.commit();
+		console.log(`${companies.length} companies inserted successfully.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
 
-// Add fields to "Companies" collection
-addDoc(companiesCollection, {
-	company_id: companyID,
-	name: faker.company.name(),
-	email: faker.internet.email(),
-	password: faker.internet.password(),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-	company_structure: faker.company.companySuffix(),
-	company_ein: faker.finance.routingNumber(),
-	company_license_number: faker.random.alphaNumeric(),
-	address: faker.address.streetAddress(),
-	phone_number: faker.phone.number(),
-	profile_picture: faker.image.imageUrl(),
-	company_website: faker.internet.url(),
-	score: scoreID,
-	orders: [orderID, orderID, orderID],
-	reviews: [reviewID, reviewID, reviewID],
-	jobs_accepted: [jobID, jobID, jobID, jobID, jobID],
-	verified: faker.datatype.boolean(),
-	slug: faker.lorem.slug(),
-	link: faker.internet.url(),
-	size: faker.datatype.number({ min: 50, max: 500 }),
-	founded: faker.datatype.number({ min: 1970, max: 2023 }),
-	social_media: faker.helpers.arrayElement([faker.internet.url(), faker.internet.url(), faker.internet.url(), faker.internet.url()]),
-});
+// Insert fake jobs
+async function insertFakeJobs(numJobs) {
+	const jobs = [];
 
-// Create "Clients" collection
-const clientsCollection = collection(db, "Clients");
+	// Get list of clients from Firestore
+	const clientsSnapshot = await db.collection("clients").get();
+	const clients = clientsSnapshot.docs.map((doc) => doc.data().client_id);
 
-// Add fields to "Clients" collection
-addDoc(clientsCollection, {
-	client_id: clientID,
-	name: faker.name.fullName(),
-	email: faker.internet.email(),
-	password: faker.internet.password(),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-	phone_number: faker.phone.number(),
-	profile_picture: faker.image.imageUrl(),
-	location: faker.address.streetAddress(),
-	preferred_services: [faker.random.word(), faker.random.word(), faker.random.word()],
-	preferred_location: faker.address.streetAddress(),
-	score: scoreID,
-	orders: [orderID, orderID, orderID],
-	reviews: [reviewID, reviewID, reviewID],
-	jobs_posted: [jobID, jobID, jobID, jobID, jobID],
-});
+	// Get list of companies from Firestore
+	const companiesSnapshot = await db.collection("companies").get();
+	const companies = companiesSnapshot.docs.map((doc) => doc.data().company_id);
 
-// Create "Jobs" collection
-const jobsCollection = collection(db, "Jobs");
+	for (let i = 0; i < numJobs; i++) {
+		const client = clients[Math.floor(Math.random() * clients.length)];
+		const company = companies[Math.floor(Math.random() * companies.length)];
 
-// Add fields to "Jobs" collection
-addDoc(jobsCollection, {
-	job_id: jobID,
-	title: faker.name.jobTitle(),
-	description: faker.lorem.sentence(),
-	posted_by: clientID,
-	accepted_by: companyID,
-	budget: faker.datatype.number({ min: 50, max: 500 }),
-	location: faker.address.streetAddress(),
-	status: "open",
-	attachments: [faker.image.imageUrl(), faker.image.imageUrl()],
-	equipment_required: [faker.random.word(), faker.random.word(), faker.random.word()],
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-	industry: faker.random.word(),
-	category: faker.random.word(),
-	tags: faker.random.word(),
-	score: scoreID,
-	slug: faker.lorem.slug(),
-	views: faker.datatype.number({ min: 1, max: 9999 }),
-	link: faker.internet.url(),
-});
+		jobs.push({
+			jobs_id: uuidv4(),
+			title: faker.lorem.words(),
+			description: faker.lorem.paragraph(),
+			posted_by: client,
+			accepted_by: company,
+			budget: faker.finance.amount(),
+			location: faker.address.city(),
+			status: "open",
+			attachments: [faker.internet.url()],
+			equipment_required: ["Hammer", "Screwdriver"],
+			created_at: new Date(),
+			updated_at: new Date(),
+			industry: "Construction",
+			category: ["Renovations"],
+			tags: ["#construction", "#renovations"],
+			slug: faker.lorem.slug(),
+			views: faker.datatype.number(),
+			link: "link is here",
+			client_id: client,
+			company_id: company,
+			score: null,
+		});
+	}
 
-// Create "Job Scores" collection
-const scoresCollection = collection(db, "Scores");
+	// Insert jobs into Firestore
+	const insertResult = await db.collection("jobs").add({ jobs });
+	console.log(`${numJobs} jobs inserted successfully.`);
+}
 
-// Add fields to "Job Scores" collection
-addDoc(scoresCollection, {
-	score_id: scoreID,
-	score: faker.datatype.number({ min: 1, max: 100 }),
-	type: faker.helpers.arrayElement([jobID, clientID, companyID]),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-});
+// Insert fake orders
+async function insertFakeOrders(numOrders) {
+	const companiesSnapshot = await db.collection("companies").get();
+	const companies = companiesSnapshot.docs.map((doc) => doc.data());
 
-// Create "Notifications" collection
-const notificationsCollection = collection(db, "Notifications");
+	const orders = [];
 
-// Add fields to "Notifications" collection
-addDoc(notificationsCollection, {
-	notification_id: notificationID,
-	recipient_id: clientID,
-	message: faker.lorem.sentence(),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-});
+	for (let i = 0; i < numOrders; i++) {
+		const company = companies[Math.floor(Math.random() * companies.length)];
 
-// Create "Ratings & Reviews" collection
-const ratingsReviewsCollection = collection(db, "Ratings & Reviews");
+		orders.push({
+			order_id: uuidv4(),
+			amount: faker.finance.amount(),
+			created_at: new Date(),
+			updated_at: new Date(),
+			company_id: company.company_id,
+		});
+	}
 
-// Add fields to "Ratings & Reviews" collection
-addDoc(ratingsReviewsCollection, {
-	review_id: reviewID,
-	reviewer_id: faker.helpers.arrayElement([companyID, clientID]),
-	reviewed_id: faker.helpers.arrayElement([clientID, companyID]),
-	rating: faker.datatype.float({ min: 1, max: 5, precision: 0.1 }),
-	review_text: faker.lorem.paragraph(),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-	review_type: faker.helpers.arrayElement(["client_review", "company_review"]),
-	slug: faker.lorem.slug(),
-	link: faker.internet.url(),
-});
+	try {
+		const batch = db.batch();
+		orders.forEach((order) => {
+			const docRef = db.collection("orders").doc(order.order_id);
+			batch.set(docRef, order);
+		});
+		await batch.commit();
+		console.log(`${orders.length} orders inserted successfully.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
 
-// Create "Orders" collection
-const ordersCollection = collection(db, "Orders");
+// Insert fake payments
+async function insertFakePayments(numPayments) {
+	const payments = [];
 
-// Add fields to "Orders" collection
-addDoc(ordersCollection, {
-	order_id: orderID,
-	company_id: companyID,
-	payment_id: paymentID,
-	job_id: jobID,
-	amount: faker.datatype.number({ min: 50, max: 1000, precision: 0.01 }),
-	payment_method: faker.helpers.arrayElement(["credit card", "PayPal", "Apple Pay"]),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-});
+	// Fetch orders data from Firestore
+	const ordersSnapshot = await db.collection("orders").get();
+	const orders = ordersSnapshot.docs.map((doc) => ({ order_id: doc.id }));
 
-// Create "Payments" collection
-const paymentsCollection = collection(db, "Payments");
+	for (let i = 0; i < numPayments; i++) {
+		const order = orders[Math.floor(Math.random() * orders.length)];
+		payments.push({
+			payment_id: uuidv4(),
+			amount: faker.finance.amount(),
+			payment_method: "Credit Card",
+			created_at: new Date(),
+			updated_at: new Date(),
+			order_id: order.order_id,
+			status: "success",
+		});
+	}
 
-// Add fields to "Payments" collection
-addDoc(paymentsCollection, {
-	payment_id: paymentID,
-	order_id: orderID,
-	company_id: companyID,
-	job_id: jobID,
-	amount: faker.datatype.number({ min: 1, max: 5000, precision: 0.01 }),
-	payment_method: faker.helpers.arrayElement(["credit card", "PayPal", "Apple Pay"]),
-	created_at: serverTimestamp(),
-	updated_at: serverTimestamp(),
-	status: faker.helpers.arrayElement(["pending", "completed", "failed"]),
-});
+	try {
+		const { writeResult } = await db.collection("payments").add(payments);
+		console.log(`${writeResult.writeResults.length} payments inserted successfully.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
 
-// Output success message
-console.log("Firebase database created successfully!");
+// Insert fake reviews
+async function insertFakeReviews(numReviews) {
+	const companiesSnapshot = await firestore.collection("companies").get();
+	const companies = companiesSnapshot.docs.map((doc) => doc.data());
+
+	const clientsSnapshot = await firestore.collection("clients").get();
+	const clients = clientsSnapshot.docs.map((doc) => doc.data());
+
+	const reviews = [];
+
+	for (let i = 0; i < numReviews; i++) {
+		const company = companies[Math.floor(Math.random() * companies.length)];
+		const client = clients[Math.floor(Math.random() * clients.length)];
+		reviews.push({
+			review_id: uuidv4(),
+			body: faker.lorem.paragraph(),
+			rating: faker.datatype.number({ min: 1, max: 5 }),
+			review_type: "company",
+			slug: faker.lorem.slug(),
+			link: faker.internet.url(),
+			created_at: new Date(),
+			updated_at: new Date(),
+			company_id: company.company_id,
+			client_id: client.client_id,
+		});
+	}
+
+	const batch = firestore.batch();
+	reviews.forEach((review) => {
+		const reviewRef = firestore.collection("reviews").doc(review.review_id);
+		batch.set(reviewRef, review);
+	});
+
+	try {
+		await batch.commit();
+		console.log(`${reviews.length} reviews inserted successfully.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// Insert fake scores
+async function insertFakeScores(numScores) {
+	const jobs = await db.collection("jobs").get();
+	const clients = await db.collection("clients").get();
+	const companies = await db.collection("companies").get();
+
+	const scores = [];
+
+	jobs.forEach((job) => {
+		clients.forEach((client) => {
+			companies.forEach((company) => {
+				scores.push({
+					score_id: uuidv4(),
+					score: faker.datatype.number({ min: 1, max: 10, precision: 1 }),
+					created_at: new Date(),
+					updated_at: new Date(),
+					job_id: job.id,
+					client_id: client.id,
+					company_id: company.id,
+				});
+			});
+		});
+	});
+
+	try {
+		const { writeBatch } = db;
+		const batch = writeBatch();
+
+		scores.forEach((score) => {
+			const scoreRef = db.collection("scores").doc(score.score_id);
+			batch.set(scoreRef, score);
+		});
+
+		await batch.commit();
+
+		console.log(`${scores.length} scores inserted successfully.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// Insert fake notifications
+async function insertFakeNotifications(numNotifications) {
+	const jobsRef = db.collection("jobs");
+	const companiesRef = db.collection("companies");
+	const clientsRef = db.collection("clients");
+
+	const jobsSnapshot = await jobsRef.get();
+	const companiesSnapshot = await companiesRef.get();
+	const clientsSnapshot = await clientsRef.get();
+
+	const jobs = jobsSnapshot.docs.map((doc) => doc.id);
+	const companies = companiesSnapshot.docs.map((doc) => doc.id);
+	const clients = clientsSnapshot.docs.map((doc) => doc.id);
+
+	const notifications = [];
+
+	for (let i = 0; i < numNotifications; i++) {
+		const job = jobs[Math.floor(Math.random() * jobs.length)];
+		const client = clients[Math.floor(Math.random() * clients.length)];
+		const company = companies[Math.floor(Math.random() * companies.length)];
+		notifications.push({
+			notification_id: uuidv4(),
+			type: "job_notification",
+			message: `You have a new job request from ${client.name} for ${job.title}`,
+			created_at: new Date(),
+			updated_at: new Date(),
+			job_id: job,
+			client_id: client,
+			company_id: company,
+		});
+	}
+
+	try {
+		const batch = db.batch();
+		notifications.forEach((notification) => {
+			const notificationRef = db.collection("notifications").doc();
+			batch.set(notificationRef, notification);
+		});
+		await batch.commit();
+		console.log(`${notifications.length} notifications inserted successfully.`);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// Call the functions to insert data into the tables
+//insertFakeClients(100);
+//insertFakeCompanies(50);
+
+// Wait for the data to be i
+//insertFakeJobs(20);
+//insertFakeOrders(50);
+//insertFakePayments(50);
+//insertFakeReviews(50);
+//insertFakeScores(50);
+insertFakeNotifications(50); 
