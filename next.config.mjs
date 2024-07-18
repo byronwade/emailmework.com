8fd0d05b116fa343d15587c2f5ef7ff6c1445e8a
Byron Wade <bcw1995@gmail.com>
2024-07-17 21:50:13 -0700

    emails

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,10 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	env: {
-		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-		SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-		SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		SUPABASE_URL: process.env.SUPABASE_URL,
+		SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+		SUPABASE_URL: process.env.SUPABASE_URL,
+		SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
 	},
 	images: {
 		remotePatterns: [
